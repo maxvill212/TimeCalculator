3dce01c89d8161c126141e0d20be4ca320607c26
Max Villeneuve <maxvilleneuve117@gmail.com>
2020-06-14 20:32:45 -0400

    Added App icon

--- app/src/main/java/com/example/timecalculator/Functions.kt
+++ app/src/main/java/com/example/timecalculator/Functions.kt
@@ -1,13 +1,19 @@
 package com.example.timecalculator
 
-
-/** Calculate minutes */
-
 fun calcWork(startHR: Int, endHR: Int, startMN: Int, endMN: Int): String {
 
     return if (endMN >= startMN){
-        (endHR - startHR).toString() + " : " + (endMN - startMN).toString()
+        var formattedMN = formatTime(endMN - startMN)
+        (endHR - startHR).toString() + " : " + formattedMN
     }else{
         (endHR - startHR - 1).toString() + " : " + (60 + (endMN - startMN)).toString()
+    }
+}
+
+fun formatTime(time: Int): String {
+    return if (time < 10){
+        "0${time}"
+    }else{
+        "$time"
     }
 }
