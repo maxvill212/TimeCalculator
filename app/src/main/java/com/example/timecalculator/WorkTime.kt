49a8b1132c2f128d2d44b83c9731cf6c39f1098a
Max Villeneuve <maxvilleneuve117@gmail.com>
2020-06-18 19:02:45 -0400

    Start on logic change

---
+++ app/src/main/java/com/example/timecalculator/WorkTime.kt
@@ -0,0 +1,14 @@
+package com.example.timecalculator
+
+class WorkTime(_hour: Int = -1, _minutes: Int = -1) {
+    private var hour = _hour
+    private var minutes = _minutes
+
+    init {
+
+    }
+
+
+
+
+}
